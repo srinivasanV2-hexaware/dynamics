12cf10129f7f5109d9de115b710ea4eaf12f50eb
Srinivasan Velu <40140@hexaware.com>
2018-04-06 16:27:46 +0530

    new commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var apicall = require("./apicall");
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-    apicall.getToken("srinivasan","velu","This is good message","9176727764","ursfriendly_sri@yahoo.com","normal","createCase");
+    apicall.getToken("Aventure","Works","This is good message","9176727764","ursfriendly_sri@yahoo.com","normal","createCase");
     res.send("Hello World");
 });
 
